23b42c51044015fde362b15af24c4cb6a34a464b
Eric Ahnell <eric.ahnell@puttysoftware.com>
2021-06-27 14:18:08 -0400

    Format code.

--- lib-images/src/com/puttysoftware/images/edges/EdgePosition.java
+++ lib-images/src/com/puttysoftware/images/edges/EdgePosition.java
@@ -6,18 +6,18 @@ Any questions should be directed to the author via email at: support@puttysoftwa
 package com.puttysoftware.images.edges;
 
 public class EdgePosition {
-  // Fields
-  private final double offset;
-  // Constants
-  public static final EdgePosition MIDDLE = new EdgePosition(0.5);
+    // Fields
+    private final double offset;
+    // Constants
+    public static final EdgePosition MIDDLE = new EdgePosition(0.5);
 
-  // Constructor
-  private EdgePosition(final double newOffset) {
-    this.offset = newOffset;
-  }
+    // Constructor
+    private EdgePosition(final double newOffset) {
+        this.offset = newOffset;
+    }
 
-  // Methods
-  public double getOffset() {
-    return this.offset;
-  }
+    // Methods
+    public double getOffset() {
+        return this.offset;
+    }
 }
--- lib-images/src/module-info.java
+++ lib-images/src/module-info.java
@@ -1,6 +1,6 @@
 module com.puttysoftware.images {
-	requires transitive java.desktop;
-	
-	exports com.puttysoftware.images;
-	exports com.puttysoftware.images.edges;
+    requires transitive java.desktop;
+
+    exports com.puttysoftware.images;
+    exports com.puttysoftware.images.edges;
 }
