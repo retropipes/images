e0c3bba2ab04a38f6d045d2e915f83dc1d76db69
Eric Ahnell <eric.ahnell@puttysoftware.com>
2021-07-03 15:31:12 -0400

    Update BufferedImageIcon to support scale factors.

--- lib-images/src/com/puttysoftware/images/BufferedImageIcon.java
+++ lib-images/src/com/puttysoftware/images/BufferedImageIcon.java
@@ -8,6 +8,7 @@ package com.puttysoftware.images;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
 import javax.swing.Icon;
@@ -15,6 +16,12 @@ import javax.swing.Icon;
 public class BufferedImageIcon extends BufferedImage implements Icon {
     // Fields
     private static final int DEFAULT_TYPE = BufferedImage.TYPE_INT_ARGB;
+    private static final int SCALE_MIN = 100;
+    private static final double SCALE_MULT = 100.0;
+    /**
+     * The global scale factor (default: 100 = 100% scaling)
+     */
+    protected static int SCALE = 100;
 
     // Constructors
     /**
@@ -57,33 +64,118 @@ public class BufferedImageIcon extends BufferedImage implements Icon {
     }
 
     /**
+     * Convenience method for scaling fixed values.
+     *
+     * @param value
+     *            the input
+     * @return the output
+     */
+    public static int getScaledValue(final int value) {
+        return (int) (value * BufferedImageIcon.SCALE
+                / BufferedImageIcon.SCALE_MULT);
+    }
+
+    /**
+     * Convenience method for determining the normalized scale.
+     *
+     * @return the normalized scale value
+     */
+    public static double getNormalizedScale() {
+        return BufferedImageIcon.SCALE / BufferedImageIcon.SCALE_MULT;
+    }
+
+    /**
+     * Convenience method for getting the scaling multiplier.
+     *
+     * @return the scaling multiplier
+     */
+    public static int getScaleMult() {
+        return (int) BufferedImageIcon.SCALE_MULT;
+    }
+
+    /**
+     * Gets the global scaling factor for image drawing.
+     *
+     * @return the global scaling factor
+     */
+    public static int getScale() {
+        return BufferedImageIcon.SCALE;
+    }
+
+    /**
+     * Sets the global scaling factor for image drawing. A value of 100 means
+     * 100% scaling (smallest allowed value). A value of 200 means 200% scaling
+     * (Apple Retina mode).
+     *
+     * @param value
+     *            the new global scaling factor
+     * @throws IllegalArgumentException
+     *             if the global scaling factor isn't valid
+     */
+    public static void setScale(final int value) {
+        if (value < BufferedImageIcon.SCALE_MIN) {
+            throw new IllegalArgumentException(Integer.toString(value));
+        }
+        BufferedImageIcon.SCALE = value;
+    }
+
+    /**
      * Paints the BufferedImageIcon, using the given Graphics, on the given
-     * Component at the given x, y location.
+     * Component at the given x, y location, using the scale factor.
      *
      * @param c
+     *            the Component to paint on
      * @param g
+     *            the Graphics to paint with
      * @param x
+     *            the horizontal (X) coordinate to start drawing
      * @param y
+     *            the vertical (Y) coordinate to start drawing
      */
     @Override
     public void paintIcon(final Component c, final Graphics g, final int x,
             final int y) {
-        g.drawImage(this, x, y, c);
+        if (BufferedImageIcon.SCALE > BufferedImageIcon.SCALE_MIN) {
+            if (g != null) {
+                final double factor = BufferedImageIcon.SCALE_MULT
+                        / BufferedImageIcon.SCALE;
+                final int width = this.getWidth(c);
+                final int height = this.getHeight(c);
+                final Graphics2D g2d = (Graphics2D) g.create(x, y, width,
+                        height);
+                g2d.scale(factor, factor);
+                g2d.drawImage(this, 0, 0, c);
+                g2d.scale(1, 1);
+                g2d.dispose();
+            }
+        } else {
+            if (g != null) {
+                g.drawImage(this, x, y, c);
+            }
+        }
     }
 
     /**
-     * @return the width of this BufferedImageIcon, in pixels
+     * Gets the pixel width of this BufferedImageIcon, adjusted for the scale
+     * factor.
+     *
+     * @return the adjusted width of this BufferedImageIcon, in pixels
      */
     @Override
     public int getIconWidth() {
-        return this.getWidth();
+        return (int) BufferedImageIcon.SCALE_MULT * this.getWidth()
+                / BufferedImageIcon.SCALE;
     }
 
     /**
-     * @return the height of this BufferedImageIcon, in pixels
+     * Gets the pixel height of this BufferedImageIcon, adjusted for the scale
+     * factor.
+     *
+     * @return the adjusted height of this BufferedImageIcon, in pixels
      */
     @Override
     public int getIconHeight() {
-        return this.getHeight();
+        return (int) BufferedImageIcon.SCALE_MULT * this.getHeight()
+                / BufferedImageIcon.SCALE;
     }
 }
